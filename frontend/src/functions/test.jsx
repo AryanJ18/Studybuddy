6b5070eb6474458e0b2a4370506e9e3374466892
AryanJ18 <18aryanj2006@gmail.com>
2026-04-22 20:05:47 +0530

    Camers feed on page

---
+++ frontend/src/functions/test.jsx
@@ -0,0 +1,20 @@
+import { useRef, useEffect } from "react"
+
+export default function Test(){
+
+  const videoRef = useRef(null);
+
+  async function setUpCamera(){
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    videoRef.current.srcObject = stream;
+  }
+
+  useEffect(()=>{
+    setUpCamera();
+  },[])
+
+  
+  return(<>
+  <video ref={videoRef} autoPlay playsInline ></video>
+  </>)
+}
