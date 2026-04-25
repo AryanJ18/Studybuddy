bc1c526422d9c90210e6913ef1eb820ded184664
AryanJ18 <18aryanj2006@gmail.com>
2026-04-26 01:33:23 +0530

    Phone detection

--- frontend/src/functions/Video.jsx
+++ frontend/src/functions/Video.jsx
@@ -1,63 +1,98 @@
 import { useRef, useEffect, useState } from "react";
-import { FaceLandmarker, FilesetResolver } from "@mediapipe/tasks-vision";
+import { FaceLandmarker, FilesetResolver, ObjectDetector } from "@mediapipe/tasks-vision";
 import { calculateEAR } from "./Ear";
 import { detectAttention } from "./Head";
 
 export default function Video(){
 
+  const faceLandmarkerRef = useRef(null);
+  const objectDetectorRef = useRef(null);
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
   const [events, setEvents] = useState([]);
   const isEyesClosedRef = useRef(false);
   const startTimeRef = useRef(null);
 
+  async function initObjectDetector() {
+    const vision = await FilesetResolver.forVisionTasks(
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+    );
+
+    objectDetectorRef.current = await ObjectDetector.createFromOptions(vision, {
+      baseOptions: {
+        modelAssetPath:
+          "https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/float16/1/efficientdet_lite0.tflite"
+      },
+      scoreThreshold: 0.5,
+      runningMode: "VIDEO"
+    });
+  }
+
+  function detectPhone(video) {
+    if (!objectDetectorRef.current) return;
+
+    const result = objectDetectorRef.current.detectForVideo(video, performance.now());
+
+    let phoneDetected = false;
+
+    result.detections.forEach((det) => {
+      if (det.categories[0].categoryName === "cell phone") {
+        phoneDetected = true;
+      }
+    });
+
+    return phoneDetected;
+  }
+
   async function init(){
     const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm");
 
-    const faceLandmarker = await FaceLandmarker.createFromOptions(vision,
+    faceLandmarkerRef.current = await FaceLandmarker.createFromOptions(vision,
     {
       baseOptions: {
         modelAssetPath:"https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task"
       },
       runningMode: "VIDEO"
     });
+  }
 
-    function loop() {
-      const video = videoRef.current;
-      const canvas = canvasRef.current;
+  function loop() {
+    const video = videoRef.current;
+    const canvas = canvasRef.current;
 
-      if (video && canvas && video.readyState >= 2) {
-        const ctx = canvas.getContext("2d");
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
+    if (video && canvas && video.readyState >= 2) {
+      const ctx = canvas.getContext("2d");
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
 
-        const result = faceLandmarker.detectForVideo(video,performance.now());
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        if (result.faceLandmarks.length > 0) {
-          const landmarks = result.faceLandmarks[0];
+      const result = faceLandmarkerRef.current.detectForVideo(video,performance.now());
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      if (result.faceLandmarks.length > 0) {
+        const landmarks = result.faceLandmarks[0];
 
-          const leftEyeIndices = [33, 160, 158, 133, 153, 144];
-          const leftEye = leftEyeIndices.map(i => landmarks[i]);
-          const ear = calculateEAR(leftEye);
-
-          console.log(detectAttention(landmarks));
-          handleEAR(ear);
-
-          landmarks.forEach((point) => {
-            ctx.beginPath();
-            ctx.arc(
-              point.x * canvas.width,
-              point.y * canvas.height,
-              2,
-              0,
-              2 * Math.PI);
-            ctx.fill();
-          });
+        const leftEyeIndices = [33, 160, 158, 133, 153, 144];
+        const leftEye = leftEyeIndices.map(i => landmarks[i]);
+        const ear = calculateEAR(leftEye);
+        console.log(detectAttention(landmarks));
+        handleEAR(ear);
+        const phone = detectPhone(video);
+        if (phone) {
+          console.log("PHONE 📱");
         }
+
+        landmarks.forEach((point) => {
+          ctx.beginPath();
+          ctx.arc(
+            point.x * canvas.width,
+            point.y * canvas.height,
+            2,
+            0,
+            2 * Math.PI);
+          ctx.fill();
+        });
       }
-      requestAnimationFrame(loop);
     }
-    loop();
+    requestAnimationFrame(loop);
   }
   
 
@@ -77,6 +112,9 @@ export default function Video(){
     async function start() {
       await setUpCamera();
       await init();
+      await initObjectDetector();
+
+      loop();
     }
     start();
   },[])
