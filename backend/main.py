d87819c9da5f44f3e72dd2cbe9139cf0ff8a1bae
AryanJ18 <18aryanj2006@gmail.com>
2026-04-27 02:56:29 +0530

    Gemini api key added

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from google import genai  # <-- Updated Import
+from dotenv import load_dotenv
+
+# uvicorn main:app --reload
+import os
+
+load_dotenv()
 
 app = FastAPI()
+
+client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
 app.add_middleware(
     CORSMiddleware,
@@ -19,12 +28,33 @@ class Summary(BaseModel):
     eyesClosedCount: int
     phoneCount: int
     distractionCount: int
-
+    
 @app.post("/report")
 async def generate_report(data: Summary):
-    print(data)
+
+    prompt = f"""
+    You are an AI productivity coach.
+
+    Analyze this focus session:
+
+    Total Time: {data.totalTime / 60000:.2f} minutes
+    Focused Time: {data.focusedTime} ms
+    Wasted Time: {data.totalWastedTime} ms
+
+    Eyes Closed: {data.eyesClosedCount}
+    Phone Usage: {data.phoneCount}
+    Distractions: {data.distractionCount}
+
+    Give:
+    1. A short performance summary
+    2. One key weakness
+    3. One actionable improvement tip
+    Keep it concise.
+    """
+
+
+    response = await client.aio.models.generate_content(model="gemini-2.5-flash",contents=prompt)
 
     return {
-        "message": "Report received successfully",
-        "data": data
+        "report": response.text
     }
